33c212a342a8bc448eec132cc548da0806b6bae2
ccha23 <42616371+ccha23@users.noreply.github.com>
2022-07-28 06:31:50 +0800

    release 0.1.4

--- divewidgets/divewidgets/_version.py
+++ divewidgets/divewidgets/_version.py
@@ -5,5 +5,5 @@
 # Distributed under the terms of the Modified BSD License.
 
 # version_info = (0, 1, 2, 'dev', 0)
-version_info = (0, 1, 2)
+version_info = (0, 1, 4)
 __version__ = ".".join(map(str, version_info))
